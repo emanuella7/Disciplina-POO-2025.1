ae60dcacbf7f22a27ac067bbe12d3c1595448e79
Manu <sousa.emanuella@aluno.ifce.edu.br>
2025-04-30 20:00:54 -0300

    Update Principal.java

--- Lista-02/Exe-resolvidos/Cap-05/Q04/src/br/edu/principal/Principal.java
+++ Lista-02/Exe-resolvidos/Cap-05/Q04/src/br/edu/principal/Principal.java
@@ -1,37 +1,62 @@
+
+
 package br.edu.principal;
 import java.util.Scanner;
 public class Principal {
+    public static void main(String[] args) {
+        double cont, cod, numVei, numAcid;
+        double maior = 0, cidMaior = 0, menor = 0, cidMenor = 0;
+        double mediaVei, somaVei = 0;
+        double mediaAcid, somaAcid = 0, contAcid = 0;
 
-	public static void main(String[] args) {
-		 Scanner dado = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-	        double sal, novo_sal, boni, aux;
-	        System.out.println("Digite seu salario: ");
-	        sal = dado.nextDouble();
+        for (cont = 1; cont <= 5; cont++) {
+            System.out.print("Digite o código da cidade: ");
+            cod = sc.nextDouble();
+            System.out.print("Digite o número de veículos: ");
+            numVei = sc.nextDouble();
+            System.out.print("Digite o número de acidentes: ");
+            numAcid = sc.nextDouble();
 
-	        if (sal <= 500) {
-	            boni = (double)  (sal * 0.05);
-	        } 
-	        
-	        else if (sal <= 1200) {
-	            boni = (double) (sal * 0.12);
-	        } 
-	        
-	        else {
-	            boni = 0;
-	        }
+            if (cont == 1) {
+                maior = numAcid;
+                cidMaior = cod;
+                menor = numAcid;
+                cidMenor = cod;
+            } else {
+                if (numAcid > maior) {
+                    maior = numAcid;
+                    cidMaior = cod;
+                }
+                if (numAcid < menor) {
+                    menor = numAcid;
+                    cidMenor = cod;
+                }
+            }
 
-	        if (sal <= 600) {
-	            aux = 150;
-	        } 
-	        
-	        else {
-	            aux = 100;
-	        }
+            somaVei = somaVei + numVei;
 
-	        novo_sal = (double) (sal + boni + aux);
-	        System.out.println(novo_sal);
+            if (numVei < 2000) {
+                somaAcid = somaAcid + numAcid;
+                contAcid = contAcid + 1;
+            }
 
-	        dado.close();
-	    }
-	}
+            System.out.println(" ");
+
+        }
+
+        System.out.println("Maior número de acidentes: " + maior + " (Cidade " + cidMaior + ")");
+        System.out.println("Menor número de acidentes: " + menor + " (Cidade " + cidMenor + ")");
+
+        mediaVei = somaVei / 5;
+        System.out.println("Média de veículos nas 5 cidades: " + mediaVei);
+
+        if (contAcid == 0) {
+            System.out.println("Não foi digitada nenhuma cidade com menos de 2000 veículos!");
+        } else {
+            mediaAcid = somaAcid / contAcid;
+            System.out.println("Média de acidentes nas cidades com menos de 2000 veículos: " + mediaAcid);
+        }
+    }
+}
