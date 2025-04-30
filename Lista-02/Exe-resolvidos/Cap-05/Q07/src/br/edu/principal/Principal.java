d2361b8bd9edc6a45da1a511bbccc5264d4d6e85
Manu <sousa.emanuella@aluno.ifce.edu.br>
2025-04-30 20:03:30 -0300

    Update Principal.java

--- Lista-02/Exe-resolvidos/Cap-05/Q07/src/br/edu/principal/Principal.java
+++ Lista-02/Exe-resolvidos/Cap-05/Q07/src/br/edu/principal/Principal.java
@@ -1,42 +1,23 @@
-package br.edu.principal;
-import java.util.Scanner;
 
+package br.edu.principal;
 public class Principal {
     public static void main(String[] args) {
-        Scanner dado = new Scanner(System.in);
+        int n1, n2, cont, res;
 
-        double a, b, c, delta, x1, x2;
+        n1 = 0;
+        n2 = 1;
 
-        System.out.print("Digite o valor de a: ");
-        a = dado.nextDouble();
+        System.out.print(n1 + " - ");
+        System.out.print(n2 + " - ");
 
-        System.out.print("Digite o valor de b: ");
-        b = dado.nextDouble();
-
-        System.out.print("Digite o valor de c: ");
-        c = dado.nextDouble();
-
-        if (a == 0) {
-            System.out.println("Estes valores não formam uma equação de segundo grau.");
-        } else {
-            delta = (b * b) - (4 * a * c);
-
-            if (delta < 0) {
-                System.out.println("Não existe raiz real.");
-            } else if (delta == 0) {
-                System.out.println("Existe uma raiz real.");
-                x1 = (-b) / (2 * a);
-                System.out.println("x = " + x1);
-            } else {
-                System.out.println("Existem duas raízes reais.");
-                x1 = (-b + Math.sqrt(delta)) / (2 * a);
-                x2 = (-b - Math.sqrt(delta)) / (2 * a);
-                System.out.println("x1 = " + x1);
-                System.out.println("x2 = " + x2);
+        for (cont = 3; cont <= 8; cont++) {
+            res = n1 + n2;
+            System.out.print(res);
+            if (cont < 8) {
+                System.out.print(" - ");
             }
+            n1 = n2;
+            n2 = res;
         }
-
-        dado.close();
     }
 }
-
