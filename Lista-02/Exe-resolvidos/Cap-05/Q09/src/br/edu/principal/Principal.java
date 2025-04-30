b32e3a13acee16c63e500674f322159132d17688
Manu <sousa.emanuella@aluno.ifce.edu.br>
2025-04-30 20:08:48 -0300

    Update Principal.java

--- Lista-02/Exe-resolvidos/Cap-05/Q09/src/br/edu/principal/Principal.java
+++ Lista-02/Exe-resolvidos/Cap-05/Q09/src/br/edu/principal/Principal.java
@@ -1,44 +1,53 @@
+
 package br.edu.principal;
 import java.util.Scanner;
-
 public class Principal {
     public static void main(String[] args) {
-        Scanner dado = new Scanner(System.in);
-        
-        double altura, peso;
+        double nota1, nota2, media, mediaClasse, totalClasse;
+        int cont, totalAprov, totalExm, totalReprov;
 
-        System.out.print("Digite a altura: ");
-        altura = dado.nextDouble();
+        Scanner sc = new Scanner(System.in);
+        totalClasse = 0;
+        totalReprov = 0;
+        totalExm = 0;
+        totalAprov = 0;
 
-        System.out.print("Digite o peso: ");
-        peso = dado.nextDouble();
+        for(cont = 1; cont <= 6; cont++) {
+            System.out.println("Aluno #" + cont);
+            System.out.print("Digite a primeira nota: ");
+            nota1 = sc.nextDouble();
+            if(nota1 < 0 || nota1 > 10){
+                System.out.print("Nota Inválida. 0 á 10!");
+                return;
+            }
 
-        if (altura < 1.20) {
-            if (peso <= 60) {
-                System.out.println("A");
-            } else if (peso <= 90) {
-                System.out.println("D");
-            } else {
-                System.out.println("G");
+            System.out.print("Digite a segunda nota: ");
+            nota2 = sc.nextDouble();
+            if(nota2 < 0 || nota2 > 10){
+                System.out.print("Nota Inválida. 0 á 10!");
+                return;
             }
-        } else if (altura <= 1.70) {
-            if (peso <= 60) {
-                System.out.println("B");
-            } else if (peso <= 90) {
-                System.out.println("E");
-            } else {
-                System.out.println("H");
+
+            media = (nota1 + nota2) / 2;
+            System.out.print("A média do Aluno #" + cont + ", é: " + media);
+            if(media <= 3){
+                totalReprov++;
+                System.out.println(" - Reprovado!");
+            } else if(media > 3 && media < 7){
+                totalExm++;
+                System.out.println(" - Exame!");
+            } else if(media >= 7){
+                totalAprov++;
+                System.out.println(" - Aprovado!");
             }
-        } else {
-            if (peso <= 60) {
-                System.out.println("C");
-            } else if (peso <= 90) {
-                System.out.println("F");
-            } else {
-                System.out.println("I");
-            }
+            totalClasse = totalClasse + media;
         }
+        System.out.println("O total de alunos Reprovados: " + totalReprov);
+        System.out.println("O total de alunos em Exame: " + totalExm);
+        System.out.println("O total de alunos Aprovados: " + totalAprov);
 
-        dado.close();
+        mediaClasse = totalClasse / 6;
+        System.out.print("A média da classe, é: " + mediaClasse);
+
     }
 }
