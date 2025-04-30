5e6b95189e19413077513a23a46ea5352ab4641e
Manu <sousa.emanuella@aluno.ifce.edu.br>
2025-04-30 20:02:41 -0300

    Update Principal.java

--- Lista-02/Exe-resolvidos/Cap-05/Q06/src/br/edu/principal/Principal.java
+++ Lista-02/Exe-resolvidos/Cap-05/Q06/src/br/edu/principal/Principal.java
@@ -1,34 +1,71 @@
+
 package br.edu.principal;
 import java.util.Scanner;
-
 public class Principal {
     public static void main(String[] args) {
-        Scanner dado = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        double pre, venda, novo_pre;
+        int codigo;
+        double nht, valor, salMin, salInicial, aux, salFinal;
+        salMin = 450;
+        char turno, categoria;
 
-        System.out.print("Digite o preço do produto: ");
-        pre = dado.nextDouble();
+        for (int cont = 1; cont <= 10; cont++) {
+            System.out.println("- Funcionário #" + cont);
 
-        System.out.print("Digite a quantidade vendida: ");
-        venda = dado.nextDouble();
-        
-        if (venda < 500 || pre < 30) {
-        	novo_pre = (double) (pre + (0.1 * pre));
-        	 System.out.println(novo_pre);
+            System.out.print("Digite o código: ");
+            codigo = sc.nextInt();
+            System.out.print("Digite o número de horas trabalhadas: ");
+            nht = sc.nextDouble();
+            System.out.print("Digite o turno (M - Matutino, V - Vespertino ou N - Noturno): ");
+            turno = sc.next().charAt(0);
+            while (turno != 'M' && turno != 'V' && turno != 'N' &&
+                    turno != 'm' && turno != 'v' && turno != 'n') {
+                System.out.print("Turno inválido. Digite novamente (M, V ou N): ");
+                turno = sc.next().charAt(0);
+            }
+
+            System.out.print("Digite a categoria (O - Operário ou G - Gerente): ");
+            categoria = sc.next().charAt(0);
+            while (categoria != 'G' && categoria != 'O' &&
+                    categoria != 'g' && categoria != 'o') {
+                System.out.print("Categoria inválida. Digite novamente (G ou O): ");
+                categoria = sc.next().charAt(0);
+            }
+
+            if (categoria == 'G') {
+                if (turno == 'N') {
+                    valor = salMin * 18/100;
+                } else {
+                    valor = salMin * 15/100;
+                }
+            } else {
+                if (turno == 'N') {
+                    valor = salMin * 13/100;
+                } else {
+                    valor = salMin * 10/100;
+                }
+            }
+
+            salInicial = nht * valor;
+
+            if (salInicial <= 300) {
+                aux = salInicial * 20/100;
+            } else if (salInicial < 600) {
+                aux = salInicial * 15/100;
+            } else {
+                aux = salInicial * 5/100;
+            }
+
+            salFinal = salInicial + aux;
+
+            System.out.println("Código: #" + codigo);
+            System.out.println("Horas trabalhadas: " + nht + "h");
+            System.out.println("Valor da hora: R$ " + valor);
+            System.out.println("Salário inicial: R$ " + salInicial);
+            System.out.println("Auxílio: R$ " + aux);
+            System.out.println("Salário final: R$ " + salFinal);
         }
-        else if ((venda >= 500 && venda < 1200) || (pre >= 30 && pre< 80)) {
-        	novo_pre = (double) (pre + (0.15 *pre));
-        	 System.out.println(novo_pre);
-        	
-        }
-        else if (venda >= 1200 || pre >= 80) {
-            novo_pre = pre - (0.20 * pre);
-            System.out.println(novo_pre);
-        }
-       
-        
-        dado.close();
-	}
-
+    }
 }
+
