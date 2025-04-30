6fe7d26699427509ac5f348450b758980a608712
Manu <sousa.emanuella@aluno.ifce.edu.br>
2025-04-30 19:57:13 -0300

    Update Principal.java

--- Lista-02/Exe-resolvidos/Cap-05/Q01/src/br/edu/principal/Principal.java
+++ Lista-02/Exe-resolvidos/Cap-05/Q01/src/br/edu/principal/Principal.java
@@ -1,40 +1,23 @@
+
+
 package br.edu.principal;
 import java.util.Scanner;
-
 public class Principal {
     public static void main(String[] args) {
-        Scanner dado = new Scanner(System.in);
+        double i, salario, novoSalario, percentual;
 
-        int hora_i, min_i, hora_f, min_f, hora_d, min_d;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite o ano atual: ");
+        int anoAtual = sc.nextInt();
 
-        System.out.println("Digite o horário inicial");
-        System.out.print("hora: ");
-        hora_i = dado.nextInt();
+        salario = 1000;
+        percentual = 1.5/100;
 
-        System.out.print("minuto: ");
-        min_i = dado.nextInt();
-
-        System.out.println("Digite o horário final");
-        System.out.print("hora: ");
-        hora_f = dado.nextInt();
-
-        System.out.print("minuto: ");
-        min_f = dado.nextInt();
-
-        if (min_i > min_f) {
-            min_f = min_f + 60;
-            hora_f = hora_f - 1;
+        novoSalario = salario + percentual * salario;
+        for(i = 2007; i <= anoAtual; i++) {
+            percentual = 2 * percentual;
+            novoSalario = novoSalario + percentual * novoSalario;
         }
-
-        if (hora_i > hora_f) {
-            hora_f = hora_f + 24;
-        }
-
-        min_d = min_f - min_i;
-        hora_d = hora_f - hora_i;
-
-        System.out.println("O jogo durou " + hora_d + " hora(s) e " + min_d + " minuto(s)");
-
-        dado.close();
+        System.out.print("O novo salário em " + anoAtual + " é, R$ " + novoSalario);
     }
 }
